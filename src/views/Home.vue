881ea926bffd278e47919f4e7c3229afbd8710f0
Ahmad Fauzan <afauzann49@gmail.com>
2021-05-24 20:15:00 +0700

    slicing

---
+++ src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="home">
+    <!-- Header Section Begin -->
+    <Header />
+    <!-- Header End -->
+
+    <!-- Hero Section Begin -->
+    <Hero />
+    <!-- Hero Section End -->
+
+    <!-- Women Banner Section Begin -->
+    <Banner />
+    <!-- Women Banner Section End -->
+
+    <!-- Instagram Section Begin -->
+    <Instagram />
+    <!-- Instagram Section End -->
+
+    <!-- Partner Section Begin -->
+    <Partner />
+    <!-- Partner Section End -->
+
+    <!-- Footer Section Begin -->
+    <Footer />
+    <!-- Footer Section End -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Header from "@/components/Header.vue";
+import Hero from "@/components/Hero.vue";
+import Banner from "@/components/Banner.vue";
+import Instagram from "@/components/Instagram.vue";
+import Partner from "@/components/Partner.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld
+    Header,
+    Hero,
+    Banner,
+    Instagram,
+    Partner,
+    Footer
+  },
+};
+</script>
